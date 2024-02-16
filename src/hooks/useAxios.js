e2ce666ea592264e702ff4d5c2ab520e37a3109c
code-washing <tadcassin225@gmail.com>
2024-02-17 01:02:07 +0600

    Add custom hooks useAuth and useAxios to project

---
+++ src/hooks/useAxios.js
@@ -0,0 +1,23 @@
+'use client';
+
+// axios
+import axios from 'axios';
+
+// server url
+import { serverUrl } from '@/uiData/serverUrl';
+
+const axiosCustom = axios.create({
+  baseURL: serverUrl,
+  withCredentials: true,
+});
+
+// const axiosSecure = axios.create({
+//   baseURL: serverUrl,
+//   withCredentials: true
+// });
+
+const useAxios = () => {
+  return { axiosCustom };
+};
+
+export default useAxios;
