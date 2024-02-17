bd4d68e44a6bd2b9ff17fc3dc2200129b5c1acd7
code-washing <tadcassin225@gmail.com>
2024-02-18 00:51:17 +0600

    Add LoginModal to Homepage

--- src/app/(main)/page.jsx
+++ src/app/(main)/page.jsx
@@ -2,21 +2,27 @@
 import InnerContainer from '@/components/containers/InnerContainer/InnerContainer';
 import Banner from '@/components/page-related/home/Banner/Banner';
 import TargetAudience from '@/components/page-related/home/TargetAudience/TargetAudience';
+import LoginModal from '@/components/forms/LoginModal/LoginModal';
 
 const Home = () => {
   return (
     <>
-      <section className='mt-custom2md mb-custom2xl'>
+      <section className='mt-custom2md mb-customXl md:mb-custom2xl'>
         <InnerContainer>
           <Banner />
         </InnerContainer>
       </section>
 
-      <section id='learn-more' className='mb-custom2xl'>
+      <section id='learn-more' className='mb-customXl md:mb-custom2xl'>
         <InnerContainer>
           <TargetAudience />
         </InnerContainer>
       </section>
+
+      {/* forms */}
+      <InnerContainer>
+        <LoginModal visible={true} />
+      </InnerContainer>
     </>
   );
 };
