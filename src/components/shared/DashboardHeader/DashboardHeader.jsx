a4ca5aae88b1bb04fb78c5de3bb0536f0d168e35
code-washing <tadcassin225@gmail.com>
2024-02-19 20:49:52 +0600

    Add DashboardLayout related header, footer, nav and mobilenav components to the project and also add custom hook useDashboardMobileNav

---
+++ src/components/shared/DashboardHeader/DashboardHeader.jsx
@@ -0,0 +1,51 @@
+'use client';
+
+// react
+import PropTypes from 'prop-types';
+
+// components
+import DashboardMobileNav from '../DashboardMobileNav/DashboardMobileNav';
+import UserProfile from '../UserProfile/UserProfile';
+import BrandLogo from '../BrandLogo/Brandlogo';
+
+// hooks
+import useFirebaseMethods from '@/hooks/useFirebaseMethods';
+
+// redux
+import { useSelector } from 'react-redux';
+
+
+
+const DashboardHeader = ({ modifyClasses = '' }) => {
+   const { profileData } = useSelector(store => store.auth);
+   const { logout } = useFirebaseMethods();
+
+   return (
+      <header
+         className={`h-[5.5rem] px-8 flex items-center bg-primary ${modifyClasses}`}
+      >
+         <div className='basis-full flex items-center justify-between'>
+            <div className='flex gap-4 items-center sm:flex-row sm:gap-6'>
+               <DashboardMobileNav
+                  navOptions={dashboardNavOptions}
+                  modifyClasses='block xl:hidden'
+                  MenuBtnModifyClasses='text-white'
+               />
+
+               <BrandLogo theme='dark' modifyClasses='h-[2.5rem]' />
+            </div>
+
+            {/* if user is truthy, show the userprofile */}
+            {profileData && (
+               <UserProfile profile={profileData} logoutFunction={logout} />
+            )}
+         </div>
+      </header>
+   );
+};
+
+DashboardHeader.propTypes = {
+   modifyClasses: PropTypes.string,
+};
+
+export default DashboardHeader;
---
+++ src/components/shared/DashboardNav/DashboardNav.jsx
@@ -0,0 +1,44 @@
+'use client';
+
+// react
+import PropTypes from 'prop-types';
+
+// next
+import Link from 'next/link';
+
+// redux
+import { useSelector } from 'react-redux';
+
+const DashboardNav = ({ modifyClasses = '' }) => {
+   const { dashboardNavOptions } = useSelector(store => store.dashboard);
+
+   console.log(dashboardNavOptions);
+
+   const linkClasses =
+      'block text-lg font-medium translate-x-0 hover:font-bold hover:translate-x-2 transition-all duration-default';
+
+   return (
+      <div className={`pl-8 pr-3 py-customSm ${modifyClasses}`}>
+         <h2 className='text-xl font-bold mb-customXs'>Dashboard</h2>
+
+         {/* the navigation menu */}
+         <ul className={`space-y-3`}>
+            {dashboardNavOptions?.map(option => {
+               return (
+                  <li key={option.id}>
+                     <Link className={linkClasses} href={option.url}>
+                        {option.text}
+                     </Link>
+                  </li>
+               );
+            })}
+         </ul>
+      </div>
+   );
+};
+
+DashboardNav.propTypes = {
+   modifyClasses: PropTypes.string,
+};
+
+export default DashboardNav;
