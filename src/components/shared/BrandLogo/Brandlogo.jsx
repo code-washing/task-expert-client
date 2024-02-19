31435cec063fadd233fda092c8661857d698520b
code-washing <tadcassin225@gmail.com>
2024-02-19 21:04:40 +0600

    Refactor code

--- src/components/shared/BrandLogo/Brandlogo.jsx
+++ src/components/shared/BrandLogo/Brandlogo.jsx
@@ -10,26 +10,26 @@ import logoPrimary from '@/assets/websiteLogo/logo-primary.webp';
 import logoWhite from '@/assets/websiteLogo/logo-white.webp';
 
 const BrandLogo = ({ modifyClasses = '', theme = 'light' }) => {
-  return (
-    <Link className={`block w-max ${modifyClasses}`} href='/'>
-      <Image
-        width={300}
-        height={100}
-        style={{ width: 'auto', height: 'inherit' }}
-        priority={true}
-        src={theme === 'light' ? logoPrimary : logoWhite}
-        alt='Company Logo'
-        className='block'
-        quality={100}
-      />
-    </Link>
-  );
+   return (
+      <Link className={`block w-max ${modifyClasses}`} href='/'>
+         <Image
+            width={500}
+            height={200}
+            style={{ width: 'auto', height: 'inherit' }}
+            priority={true}
+            src={theme === 'light' ? logoPrimary : logoWhite}
+            alt='Company Logo'
+            className='block'
+            quality={100}
+         />
+      </Link>
+   );
 };
 
 BrandLogo.propTypes = {
-  modifyClasses: PropTypes.string,
-  imageModifyClasses: PropTypes.string,
-  theme: PropTypes.string,
+   modifyClasses: PropTypes.string,
+   imageModifyClasses: PropTypes.string,
+   theme: PropTypes.string,
 };
 
 export default BrandLogo;
