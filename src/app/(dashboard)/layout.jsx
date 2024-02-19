3c388bfd1250448691411081471da8add4b2613c
code-washing <tadcassin225@gmail.com>
2024-02-20 04:46:17 +0600

    Add headings and pinned task components to DashboardNav, and change styles of DashboardNav in DashboardLayout

--- src/app/(dashboard)/layout.jsx
+++ src/app/(dashboard)/layout.jsx
@@ -8,9 +8,9 @@ import DashboardNav from '@/components/shared/DashboardNav/DashboardNav';
 const DashboardLayout = ({ children }) => {
    return (
       <div className='max-w-[120rem] mx-auto h-screen grid grid-cols-1 lg:grid-cols-[18rem_1fr] xl:grid-cols-[22.5rem_1fr]'>
-         <DashboardNav modifyClasses='hidden lg:block bg-lightGray border-r border-lightBorder' />
+         <DashboardNav modifyClasses='hidden lg:block border-r border-lightBorder' />
 
-         <div className='h-full bg-blackLight'>
+         <div className='h-full'>
             <DashboardHeader />
 
             <div className='h-[calc(100vh-5.8rem)] overflow-y-auto'>
