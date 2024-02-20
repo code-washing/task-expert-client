d698e206079345d546949beeb7ecafc06598d195
code-washing <tadcassin225@gmail.com>
2024-02-20 20:11:03 +0600

    Add background theme to the dashboard layout

--- src/app/(dashboard)/layout.jsx
+++ src/app/(dashboard)/layout.jsx
@@ -13,7 +13,12 @@ const DashboardLayout = ({ children }) => {
          <div className='h-full'>
             <DashboardHeader />
 
-            <div className='h-[calc(100vh-5.8rem)] overflow-y-auto'>
+            <div
+               style={{
+                  backgroundImage: `url('data:image/svg+xml;utf8,<svg viewBox="0 0 1270 500" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path fill="rgba(255, 134, 49, 1)" d="M 0 500 C 260.8 500 391.2 340 652 340 L 652 340 L 652 0 L 0 0 Z" stroke-width="0"></path> <path fill="rgba(255, 134, 49, 1)" d="M 651 340 C 898.6 340 1022.4 211 1270 211 L 1270 211 L 1270 0 L 651 0 Z" stroke-width="0"></path> </svg>')`,
+               }}
+               className='h-[calc(100vh-5.8rem)] relative overflow-y-auto !bg-cover !bg-no-repeat !bg-center'
+            >
                {/* page  */}
                {children}
             </div>
