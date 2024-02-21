f5f2b9a900c9e4b8bde7a9dcbe8900fe924d06c3
code-washing <tadcassin225@gmail.com>
2024-02-21 19:58:12 +0600

    Implement useClickOutside hook in MobileNav, DashboardMobileNav and place DashboardNavConent inside DashboardMobileNav

--- src/components/page-related/dashboard-home/DashboardMobileNav/DashboardMobileNav.jsx
+++ src/components/page-related/dashboard-home/DashboardMobileNav/DashboardMobileNav.jsx
@@ -3,22 +3,18 @@
 // react
 import PropTypes from 'prop-types';
 
-// next
-import Link from 'next/link';
-
 // components
 import MobileMenuCloseBtn from '@/components/shared/MobileMenuCloseBtn/MobileMenuCloseBtn';
 import MobileMenuBtn from '@/components/shared/MobileMenuBtn/MobileMenuBtn';
 import ButtonBtn from '@/components/shared/ButtonBtn/ButtonBtn';
 import BrandLogo from '@/components/shared/BrandLogo/BrandLogo';
+import DashboardNavContent from '../DashboardNavContent/DashboardNavContent';
 
 // hook
 import useDashboardMobileNav from '@/hooks/useDashboardMobileNav';
 import useEscapeClose from '@/hooks/useEscapeClose';
 import useFirebaseMethods from '@/hooks/useFirebaseMethods';
-
-// redux
-import { useSelector } from 'react-redux';
+import useClickOutside from '@/hooks/useClickOutside';
 
 const DashboardMobileNav = ({
    modifyClasses = '',
@@ -31,17 +27,18 @@ const DashboardMobileNav = ({
       closeDashboardMobileNav,
    } = useDashboardMobileNav();
    const { logout } = useFirebaseMethods();
-   const { dashboardNavOptions } = useSelector(store => store.dashboard);
 
-   // add escape key close functionality
+   const handleClickOutside = e => {
+      if (!e.target.closest('.dashboard-mobilenav-focus')) {
+         closeDashboardMobileNav();
+      }
+   };
+
+   useClickOutside(dashboardMobileNavOpen, handleClickOutside);
    useEscapeClose(closeDashboardMobileNav);
 
-   // common link casses
-   const linkClasses =
-      'leading-[normal] px-2 py-1 rounded-default text-white hover:text-primaryLightest font-medium transition-all duration-200';
-
    return (
-      <div className={`${modifyClasses}`}>
+      <div className={`dashboard-mobilenav-focus ${modifyClasses}`}>
          <MobileMenuBtn
             modifyClasses={MenuBtnModifyClasses}
             openNavFunction={openDashboardMobileNav}
@@ -49,9 +46,9 @@ const DashboardMobileNav = ({
 
          {/* mobile navigation */}
          <nav
-            className={`block h-screen fixed top-0 left-0 w-full sm:w-[50%] md:w-[40%] lg:w-[35%] 2xl:w-[20%] -translate-x-full origin-center transition-all duration-default z-40 overflow-x-hidden ${
+            className={`block h-screen fixed top-0 left-0 w-full xs:w-[70%] sm:w-[50%] md:w-[40%] lg:w-[35%] 2xl:w-[20%] -translate-x-full origin-center transition-all duration-default z-40 overflow-x-hidden ${
                dashboardMobileNavOpen ? '!translate-x-0' : ''
-            } p-8 bg-gradient-to-bl from-secondary to-primary`}
+            } p-6 bg-white`}
          >
             {/* close nav button */}
             <MobileMenuCloseBtn
@@ -62,28 +59,14 @@ const DashboardMobileNav = ({
             {/* brandlogo */}
             <BrandLogo
                onClickFunction={closeDashboardMobileNav}
-               theme='dark'
-               modifyClasses='h-[3rem] mb-customXs'
+               modifyClasses='h-[3rem] mb-12'
             />
 
-            {/* the navigation menu */}
-            <ul className='flex flex-col gap-3'>
-               {/* this part will be always shown */}
-               {dashboardNavOptions?.options?.map((option, i) => {
-                  return (
-                     <li key={i} onClick={closeDashboardMobileNav}>
-                        <Link className={linkClasses} href={option.url}>
-                           {option.text}
-                        </Link>
-                     </li>
-                  );
-               })}
-            </ul>
+            <DashboardNavContent />
 
             {/* signout button */}
             <ButtonBtn
                text='Sign Out'
-               colorTheme='outlined'
                onClickFunction={() => {
                   logout();
                   closeDashboardMobileNav();
