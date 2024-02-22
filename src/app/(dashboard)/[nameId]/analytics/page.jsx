7ee6720c672d9b9e382c8ffc482515587c930c5a
code-washing <tadcassin225@gmail.com>
2024-02-22 12:48:16 +0600

    Add UnderDevelopment component to project and apply it to unavailable pages

---
+++ src/app/(dashboard)/[nameId]/analytics/page.jsx
@@ -0,0 +1,11 @@
+import UnderDevelopment from '@/components/shared/UnderDevelopment/UnderDevelopment';
+
+const page = () => {
+   return (
+      <div>
+         <UnderDevelopment />
+      </div>
+   );
+};
+
+export default page;
---
+++ src/components/shared/UnderDevelopment/UnderDevelopment.jsx
@@ -0,0 +1,11 @@
+const UnderDevelopment = () => {
+   return (
+      <div className='my-[10rem] mx-auto shadow-medium rounded-xl flex bg-white w-max h-auto justify-center items-center p-10'>
+         <p className='text-primary font-medium text-center text-2xl'>
+            Currently under development. Please check back later.
+         </p>
+      </div>
+   );
+};
+
+export default UnderDevelopment;
