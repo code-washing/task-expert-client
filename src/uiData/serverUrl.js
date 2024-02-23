b7193a40019b8e4f85fa198347054c157a41f0b7
code-washing <tadcassin225@gmail.com>
2024-02-24 01:19:11 +0600

    Refactor code

--- src/uiData/serverUrl.js
+++ src/uiData/serverUrl.js
@@ -1,2 +1,2 @@
-export const serverUrl = process.env.NEXT_PUBLIC_PROD_SERVER_URL;
-// export const serverUrl = process.env.NEXT_PUBLIC_DEV_SERVER_URL;
+// export const serverUrl = process.env.NEXT_PUBLIC_PROD_SERVER_URL;
+export const serverUrl = process.env.NEXT_PUBLIC_DEV_SERVER_URL;
