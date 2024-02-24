559af02aac12b7aad7ada7ceaa2b9392081dcfe6
code-washing <tadcassin225@gmail.com>
2024-02-25 05:03:16 +0600

    Add new animation keyframes in tailwind config, add animations to Homepage Banner section, delete banner video from project

--- src/components/page-related/home/Banner/Banner.jsx
+++ src/components/page-related/home/Banner/Banner.jsx
@@ -15,16 +15,20 @@ const Banner = () => {
    return (
       <div className='grid grid-cols-1 gap-12 sm:gap-[5rem] lg:gap-0 lg:grid-cols-2 items-center'>
          {/* banner text part */}
-         <BannerText heading={heading} subheading={subheading} />
+         <BannerText
+            heading={heading}
+            subheading={subheading}
+            modifyClasses='lg:animate-fadeInFromLeft'
+         />
 
          {/* banner image part */}
-         <div className='w-full h-full sm:w-[80%] lg:w-full mx-auto'>
+         <div className='w-full h-full sm:w-[80%] lg:w-full mx-auto animate-[fade-in_600ms_ease-out_forwards] lg:animate-fadeInFromRight'>
             <Image
                width={600}
                height={450}
                priority={true}
                src={bannerImg}
-               className='!w-full !h-full !object-cover rounded-l-full rounded-tr-full lg:rounded-tr-none shadow-medium'
+               className='!w-full !h-full !object-cover rounded-l-full rounded-tr-full shadow-lg'
                alt='Woman working on a laptop'
             />
          </div>
--- src/components/page-related/home/Banner/BannerText/BannerText.jsx
+++ src/components/page-related/home/Banner/BannerText/BannerText.jsx
@@ -4,9 +4,11 @@ import PropTypes from 'prop-types';
 // component
 import BannerTextBtns from './BannerTextBtns/BannerTextBtns';
 
-function BannerText({ heading, subheading }) {
+function BannerText({ heading, subheading, modifyClasses = '' }) {
    return (
-      <div className='w-full h-full flex items-center text-center lg:text-left'>
+      <div
+         className={`w-full h-full flex items-center text-center lg:text-left ${modifyClasses}`}
+      >
          <div className='w-full lg:w-[80%]'>
             {/* text part */}
             <h1 className='font-bold text-3xl sm:text-4xl md:text-[2.6rem] xl:text-5xl 3xl:text-6xl mb-4 2md:mb-5 !leading-snug'>
@@ -27,6 +29,7 @@ function BannerText({ heading, subheading }) {
 BannerText.propTypes = {
    heading: PropTypes.node,
    subheading: PropTypes.node,
+   modifyClasses: PropTypes.string,
 };
 
 export default BannerText;
