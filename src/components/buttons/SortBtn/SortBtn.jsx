cb2ddd7d5175e47069bd4efd749c50eede19a770
code-washing <tadcassin225@gmail.com>
2024-03-02 23:50:22 +0600

    Add renderChildren render prop to SortBtn and refactor the component's styles

--- src/components/buttons/SortBtn/SortBtn.jsx
+++ src/components/buttons/SortBtn/SortBtn.jsx
@@ -1,23 +1,38 @@
 'use client';
 
 // react
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 
 // icon
 import { Icon } from '@iconify/react';
 
-const SortBtn = ({ text = 'Sort', modifyClasses = '' }) => {
+
+const SortBtn = ({ text = 'Sort', renderChildren, modifyClasses = '' }) => {
+   const [showMenu, setShowMenu] = useState(false);
+
+   const handleClick = () => {
+      setShowMenu(prev => !prev);
+   };
+
+
    return (
+      <div className={`relative shadow-sm ${modifyClasses}`}>
       <button
-         className={`flex gap-2 items-center rounded-default bg-white hover:text-primary border border-neutral-300 shadow-sm px-2 py-1 transition-all duration-default ${modifyClasses}`}
+         className={`flex gap-2 items-center rounded-default bg-white hover:text-primary hover:border-primary border border-neutral-300 shadow-sm px-2 py-1 transition-all duration-default`}
       >
          <Icon className='text-xl' icon='ph:sort-descending-light' />{' '}
          <p className='hidden md:block text-lg'>{text}</p>
       </button>
+
+      {renderChildren && renderChildren(showMenu, setShowMenu)}
+      </div>
    );
 };
 
 SortBtn.propTypes = {
+   text: PropTypes.string,
+   renderChildren: PropTypes.func,
    modifyClasses: PropTypes.string,
 };
 
