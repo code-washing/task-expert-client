86a7e9395c9321276a0a0528a76e5ef7f4430c48
code-washing <tadcassin225@gmail.com>
2024-03-03 00:00:20 +0600

    Fix border radius of SortBtn

--- src/components/buttons/SortBtn/SortBtn.jsx
+++ src/components/buttons/SortBtn/SortBtn.jsx
@@ -18,7 +18,7 @@ const SortBtn = ({ text = 'Sort', renderChildren, modifyClasses = '' }) => {
       <div className={`relative shadow-sm ${modifyClasses}`}>
          <button
             onClick={handleClick}
-            className={`flex gap-2 items-center rounded-default bg-white hover:text-primary hover:border-primary border border-neutral-300 shadow-sm px-2 py-1 transition-all duration-default`}
+            className={`flex gap-2 items-center rounded-[inherit] bg-white hover:text-primary hover:border-primary border border-neutral-300 shadow-sm px-2 py-1 transition-all duration-default`}
          >
             <Icon className='text-xl' icon='ph:sort-descending-light' />{' '}
             <p className='hidden md:block text-lg'>{text}</p>
