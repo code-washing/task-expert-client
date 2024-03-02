813e0835c8782800dab12777c37edea7b08bb3d3
code-washing <tadcassin225@gmail.com>
2024-03-02 23:53:24 +0600

    Refactor code

--- src/components/buttons/SortBtn/SortBtn.jsx
+++ src/components/buttons/SortBtn/SortBtn.jsx
@@ -7,7 +7,6 @@ import PropTypes from 'prop-types';
 // icon
 import { Icon } from '@iconify/react';
 
-
 const SortBtn = ({ text = 'Sort', renderChildren, modifyClasses = '' }) => {
    const [showMenu, setShowMenu] = useState(false);
 
@@ -15,17 +14,17 @@ const SortBtn = ({ text = 'Sort', renderChildren, modifyClasses = '' }) => {
       setShowMenu(prev => !prev);
    };
 
-
    return (
       <div className={`relative shadow-sm ${modifyClasses}`}>
-      <button
-         className={`flex gap-2 items-center rounded-default bg-white hover:text-primary hover:border-primary border border-neutral-300 shadow-sm px-2 py-1 transition-all duration-default`}
-      >
-         <Icon className='text-xl' icon='ph:sort-descending-light' />{' '}
-         <p className='hidden md:block text-lg'>{text}</p>
-      </button>
+         <button
+            onClick={handleClick}
+            className={`flex gap-2 items-center rounded-default bg-white hover:text-primary hover:border-primary border border-neutral-300 shadow-sm px-2 py-1 transition-all duration-default`}
+         >
+            <Icon className='text-xl' icon='ph:sort-descending-light' />{' '}
+            <p className='hidden md:block text-lg'>{text}</p>
+         </button>
 
-      {renderChildren && renderChildren(showMenu, setShowMenu)}
+         {renderChildren && renderChildren(showMenu, setShowMenu)}
       </div>
    );
 };
