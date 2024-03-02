5cf5dd6feba37cd86b6cadcfb28b9de8508b6c3c
code-washing <tadcassin225@gmail.com>
2024-03-02 23:06:32 +0600

    Refactor code

--- src/components/buttons/FilterBtn/FilterBtn.jsx
+++ src/components/buttons/FilterBtn/FilterBtn.jsx
@@ -2,22 +2,36 @@
 
 // react
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 
 // icon
 import { Icon } from '@iconify/react';
 
-const FilterBtn = ({ text = 'Filter', modifyClasses = '' }) => {
+const FilterBtn = ({ text = 'Filter', renderChildren, modifyClasses = '' }) => {
+   const [showMenu, setShowMenu] = useState(false);
+
+   const handleClick = () => {
+      setShowMenu(prev => !prev);
+   };
+
    return (
-      <button
-         className={`flex gap-2 items-center rounded-default bg-white hover:text-primary border border-neutral-300 shadow-sm px-2 py-1 transition-all duration-default ${modifyClasses}`}
-      >
-         <Icon className='text-xl' icon='ph:sliders-light' />{' '}
-         <p className='hidden md:block text-lg'>{text}</p>
-      </button>
+      <div className={`relative shadow-sm  ${modifyClasses}`}>
+         <button
+            onClick={handleClick}
+            className='flex gap-2 bg-white hover:text-primary hover:border-primary border border-neutral-300 items-center rounded-[inherit] px-2 py-1 transition-all duration-default'
+         >
+            <Icon className='text-xl' icon='ph:sliders-light' />{' '}
+            <p className='hidden md:block text-lg'>{text}</p>
+         </button>
+
+         {renderChildren && renderChildren(showMenu, setShowMenu)}
+      </div>
    );
 };
 
 FilterBtn.propTypes = {
+   text: PropTypes.string,
+   renderChildren: PropTypes.func,
    modifyClasses: PropTypes.string,
 };
 
--- src/components/shared/MenuPanel/MenuPanel.jsx
+++ src/components/shared/MenuPanel/MenuPanel.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 // hooks
 import useClickOutside from '../../../hooks/useClickOutside';
 
-const MenuPanel = ({ show = false, setShow, children }) => {
+const MenuPanel = ({ show = false, setShow, children, modifyClasses = '' }) => {
    const handleClickOutside = e => {
       if (!e.target.closest('.menu-panel-focus')) {
          setShow(false);
@@ -14,9 +14,9 @@ const MenuPanel = ({ show = false, setShow, children }) => {
 
    return (
       <div
-         className={`absolute w-max top-3/4 right-0 z-30 bg-white border border-neutral-300 shadow-xl rounded-xl p-4 px-5 space-y-4 menu-panel-focus ${
+         className={`absolute w-max top-3/4 right-0 z-30 bg-white border border-neutral-300 shadow-md rounded-xl p-4 px-5 space-y-4 menu-panel-focus ${
             show ? 'block' : 'hidden'
-         }`}
+         } ${modifyClasses}`}
       >
          {children}
       </div>
@@ -27,6 +27,7 @@ MenuPanel.propTypes = {
    show: PropTypes.bool,
    setShow: PropTypes.func,
    children: PropTypes.any,
+   modifyClasses: PropTypes.string,
 };
 
 export default MenuPanel;
