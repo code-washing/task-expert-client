dcaea7cc33d45e27340c232b09ecafe7062208ab
code-washing <tadcassin225@gmail.com>
2024-03-03 14:27:51 +0600

    Create HeaderAuthBtns and add it to Header

--- src/components/shared/Header/Header.jsx
+++ src/components/shared/Header/Header.jsx
@@ -3,91 +3,18 @@
 // react
 import PropTypes from 'prop-types';
 
-// next
-import Link from 'next/link';
-
 // components
 import BrandLogo from '@/components/shared/BrandLogo/BrandLogo';
 import InnerContainer from '@/components/containers/InnerContainer/InnerContainer';
 import MobileNav from '@/components/shared/MobileNav/MobileNav';
-import LoadingSpinner from '../LoadingSpinner/LoadingSpinner';
-
-// hooks
-import useFormVisiblity from '@/hooks/useFormVisiblity';
-import useFirebaseMethods from '@/hooks/useFirebaseMethods';
-
-// redux
-import { useSelector } from 'react-redux';
+import HeaderAuthBtns from '../HeaderAuthBtns/HeaderAuthBtns';
 
 const Header = ({ modifyClasses = '' }) => {
-   const { profileData, userLoading } = useSelector(store => store.auth);
-   const { openLoginFormWithBackdrop, openSignupFormWithBackdrop } =
-      useFormVisiblity();
-   const { logout } = useFirebaseMethods();
-
-   // common btn classes
-   const btnClasses = 'hover:text-primary transition-all duration-default';
-
    return (
-      <header className={`pt-customXs pb-customXs ${modifyClasses}`}>
+      <header className={`pt-5 lg:pt-6 ${modifyClasses}`}>
          <InnerContainer>
             {/* login/ register/ account name */}
-            <div
-               className={`flex ${
-                  profileData
-                     ? 'flex-col gap-2 sm:flex-row sm:gap-4'
-                     : 'flex-row gap-4'
-               } justify-center items-center 2md:justify-end lg:text-lg mb-customXs 2md:mb-custom2xs font-medium`}
-            >
-               {userLoading && (
-                  <LoadingSpinner
-                     text='Loading User'
-                     textSizeClass='lg:!text-lg !text-primary'
-                     loaderSizeClass='!hidden'
-                  />
-               )}
-
-               {/* if no user then login and registration btns are shown */}
-               {!userLoading && !profileData && (
-                  <>
-                     <button
-                        onClick={openLoginFormWithBackdrop}
-                        className={btnClasses}
-                     >
-                        Login
-                     </button>
-
-                     <button
-                        onClick={openSignupFormWithBackdrop}
-                        className={btnClasses}
-                     >
-                        Register
-                     </button>
-                  </>
-               )}
-
-               {/* if user available then show name and go to dashboard and logout btns */}
-               {!userLoading && profileData && (
-                  <>
-                     <p>
-                        Logged in as:{' '}
-                        <span className='font-semibold'>
-                           {profileData.name}
-                        </span>
-                     </p>
-
-                     <Link
-                        href={'/manage-tasks'}
-                        className={`${btnClasses} underline text-primary`}
-                     >
-                        Visit Dashboard
-                     </Link>
-                     <button onClick={logout} className={btnClasses}>
-                        Sign Out
-                     </button>
-                  </>
-               )}
-            </div>
+            <HeaderAuthBtns modifyClasses='mb-5' />
 
             <div className='grid grid-cols-[1fr_3fr_1fr] 2md:grid-cols-2 items-center'>
                {/* div for design purpose */}
