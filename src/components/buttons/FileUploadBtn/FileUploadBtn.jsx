4db80acb9ba338e999aac52fab95437e3e05d589
code-washing <tadcassin225@gmail.com>
2024-03-05 20:52:53 +0600

    Change FileUploadBtn styles

--- src/components/buttons/FileUploadBtn/FileUploadBtn.jsx
+++ src/components/buttons/FileUploadBtn/FileUploadBtn.jsx
@@ -1,36 +1,59 @@
 // react
-import PropTypes from "prop-types";
-import { useState } from "react";
+import PropTypes from 'prop-types';
+import { useState } from 'react';
 
-const FileUploadBtn = ({ children = null, modifyClasses = "" }) => {
-  const [selected, setSelected] = useState(false);
+const FileUploadBtn = ({
+   children = null,
+   colorTheme = 'primary',
+   modifyClasses = '',
+}) => {
+   const [selected, setSelected] = useState(false);
 
-  return (
-    <div
-      className={`rounded-defaultLg bg-primary hover:bg-primaryLight transition-all duration-200 text-white px-4 py-2 pointer cursor-pointer ${modifyClasses}`}
-    >
-      <label className="cursor-pointer">
-        <span className="flex w-full items-center justify-center gap-2">
-          {!selected && !children && "Upload File"}
-          {!selected && children && children}
-          {selected && "Selected"}
-        </span>
-        <input
-          onChange={() => {
-            setSelected(true);
-          }}
-          className="hidden"
-          type="file"
-          name="file"
-        />
-      </label>
-    </div>
-  );
+   const primaryColors =
+      'bg-primary border-primary hover:bg-primaryLight hover:border-primaryLight text-white';
+
+   const primaryOutlined =
+      'border-primary hover:border-black text-primary hover:text-black';
+
+   return (
+      <div
+         className={`rounded-defaultLg border transition-all duration-default px-4 py-3 cursor-pointer ${
+            colorTheme === 'primary'
+               ? primaryColors
+               : colorTheme === 'primaryOutlined'
+               ? primaryOutlined
+               : ''
+         } ${modifyClasses}`}
+      >
+         <label
+            style={{ fontSize: 'inherit' }}
+            className='cursor-pointer'
+         >
+            <span
+               style={{ fontSize: 'inherit' }}
+               className='flex w-full items-center justify-center gap-2'
+            >
+               {!selected && !children && 'Upload File'}
+               {!selected && children && children}
+               {selected && 'Selected'}
+            </span>
+            <input
+               onChange={() => {
+                  setSelected(true);
+               }}
+               className='hidden'
+               type='file'
+               name='file'
+            />
+         </label>
+      </div>
+   );
 };
 
 FileUploadBtn.propTypes = {
-  children: PropTypes.any,
-  modifyClasses: PropTypes.string,
+   children: PropTypes.any,
+   colorTheme: PropTypes.string,
+   modifyClasses: PropTypes.string,
 };
 
 export default FileUploadBtn;
