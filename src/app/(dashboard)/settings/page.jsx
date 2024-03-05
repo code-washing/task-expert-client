601cbd3cfd4e3d5e16443fad85405669a0619119
code-washing <tadcassin225@gmail.com>
2024-03-05 23:50:23 +0600

    Change settings page code structure using new components

--- src/app/(dashboard)/settings/page.jsx
+++ src/app/(dashboard)/settings/page.jsx
@@ -1,14 +1,21 @@
 // component
-import ProfileInformation from '@/components/page-related/settings/ProfileInformation/ProfileInformation';
+import ProfileInfoChanger from '@/components/page-related/settings/ProfileInfoChanger/ProfileInfoChanger';
+import ProfilePhotoChanger from '@/components/page-related/settings/ProfilePhotoChanger/ProfilePhotoChanger';
 
 const page = () => {
    return (
       <div className='h-full flex flex-col'>
-         <div className='mx-4 md:mx-8 xl:mx-16 bg-white my-8 md:my-12 lg:my-14 xl:my-16 rounded-xl shadow-medium overflow-hidden'>
-            {/* main heading */}
-            <h2 className='bg-primary text-white font-semibold leading-none text-xl p-4 text-center'>Profile</h2>
+         <div className='mx-4 md:mx-8 xl:mx-16 bg-white my-8 md:my-12 lg:my-14 xl:my-16 rounded-xl shadow-small'>
 
-            <ProfileInformation /> 
+            {/* main heading */}
+            <h2 className='bg-primary rounded-t-xl text-white font-semibold leading-none text-xl p-3 text-center'>
+               Profile
+            </h2>
+
+
+            <ProfilePhotoChanger modifyClasses='my-8' />
+
+            <ProfileInfoChanger />
          </div>
       </div>
    );
