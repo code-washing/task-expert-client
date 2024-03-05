e5f5c6bb7b6d57bde1b6794bf8e4e018ce342f53
code-washing <tadcassin225@gmail.com>
2024-03-05 21:31:30 +0600

    Remove svg primary background from overall dashboard layout and place it only in manage-tasks page

--- src/app/(dashboard)/manage-tasks/page.jsx
+++ src/app/(dashboard)/manage-tasks/page.jsx
@@ -10,7 +10,12 @@ import TaskDetailsPanel from '@/components/page-related/manage-tasks/TaskDetails
 
 const ManageTasks = () => {
    return (
-      <div className='h-full flex flex-col'>
+      <div
+         style={{
+            backgroundImage: `url('data:image/svg+xml;utf8,<svg viewBox="0 0 1270 500" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path fill="rgba(255, 134, 49, 1)" d="M 0 438 C 255.60000000000002 438 383.4 281 639 281 L 639 281 L 639 0 L 0 0 Z" stroke-width="0"></path> <path fill="rgba(255, 134, 49, 1)" d="M 638 281 C 890.8 281 1017.2 131 1270 131 L 1270 131 L 1270 0 L 638 0 Z" stroke-width="0"></path> </svg>')`,
+         }}
+         className='h-full flex flex-col !bg-cover !bg-no-repeat !bg-center'
+      >
          {/* make this section's postion relative for the forms below */}
          <section className='mb-8 md:mb-12 lg:mb-14 xl:mb-16 relative'>
             <TaskUtilsHeader />
