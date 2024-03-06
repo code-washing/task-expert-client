4e7a5b718148d36fe1ff8ffb21dbddcb6a4eb5a3
code-washing <tadcassin225@gmail.com>
2024-03-06 23:54:13 +0600

    Add label prop and labeltext to inputField1 component

--- src/components/shared/InputField1/InputField1.jsx
+++ src/components/shared/InputField1/InputField1.jsx
@@ -2,22 +2,28 @@ import PropTypes from 'prop-types';
 
 const InputField1 = ({
    name,
+   label,
    type = 'text',
    placeholder = 'Default Placeholder',
    modifyClasses = '',
 }) => {
    return (
-      <input
-         name={name}
-         type={type}
-         placeholder={placeholder}
-         className={`block w-full rounded-default border border-textLight py-2 px-4 text-textPrimary ${modifyClasses}`}
-      />
+      <label className={`w-full text-textPrimary ${modifyClasses}`}>
+         {label && <span className='block font-bold text-sm sm:text-base mb-2 md:mb-4'>{label}</span>}
+
+         <input
+            name={name}
+            type={type}
+            placeholder={placeholder}
+            className='block w-full rounded-default border text-sm sm:text-base border-neutral-400 py-2 px-4'
+         />
+      </label>
    );
 };
 
 InputField1.propTypes = {
    name: PropTypes.string,
+   label: PropTypes.string,
    type: PropTypes.string,
    placeholder: PropTypes.string,
    modifyClasses: PropTypes.string,
