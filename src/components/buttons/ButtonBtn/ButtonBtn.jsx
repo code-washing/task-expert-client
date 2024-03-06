8507a272b3eed80510b744479ef907860b0c3117
code-washing <tadcassin225@gmail.com>
2024-03-06 22:21:19 +0600

    Add common color theme styles to global.css and create ProfilePhotoChangeBtn, refactor ButtonBtn and LinkBtn components

--- src/components/buttons/ButtonBtn/ButtonBtn.jsx
+++ src/components/buttons/ButtonBtn/ButtonBtn.jsx
@@ -7,34 +7,16 @@ import LoadingSpinner from '../../shared/LoadingSpinner/LoadingSpinner';
 const ButtonBtn = ({
    text,
    onClickFunction = null,
-   colorTheme = '',
+   colorTheme = 'primary',
    modifyClasses = '',
-   theme = 'light',
+   // theme = 'light',
    loading = false,
    disabled = false,
 }) => {
-   // common classes
-   const outlinedClasses = `bg-transparent border border-gray-300 hover:border-white text-gray-300 hover:text-white`;
+   
+   const colorThemeClass = `${colorTheme}Classes`;
 
-   const oulinedPrimaryClasses = `bg-transparent border ${
-      theme === 'dark'
-         ? 'border-primaryDark text-primaryDark hover:text-primary hover:border-primary'
-         : 'border-primary text-primary hover:text-primaryDark hover:border-primaryDark'
-   } disabled:border-primary disabled:text-primary`;
-
-   const primaryClasses = `border text-white ${
-      theme === 'dark'
-         ? 'bg-primaryDark border-primaryDark hover:bg-primary hover:border-primary'
-         : 'bg-primary border-primary hover:bg-primaryDark hover:border-primaryDark'
-   } disabled:bg-primary disabled:border-primary`;
-
-   const blackClasses =
-      'bg-blackLight border border-blackLight hover:bg-textPrimary hover:border-textPrimary text-white disabled:bg-blackLight';
-
-   const dangerClasses =
-      'bg-red-700 border border-red-700 hover:bg-red-500 hover:border-red-500 text-white disabled:bg-red-700 disabled:border-red-700';
-
-   const allClasses = `block min-w-[8rem] w-max capitalize transition-all duration-default rounded-defaultLg text-center px-6 py-3 3xl:text-xl  active:scale-[0.98] disabled:opacity-60 disabled:scale-100 disabled:cursor-not-allowed font-medium ${modifyClasses}`;
+   const allClasses = `block min-w-[6rem] w-max capitalize transition-all duration-default rounded-defaultLg text-center text-sm sm:text-base px-4 py-2 active:scale-[0.98] disabled:opacity-60 disabled:scale-100 disabled:cursor-not-allowed font-medium border ${colorThemeClass} ${modifyClasses}`;
 
    return (
       <button
@@ -42,17 +24,7 @@ const ButtonBtn = ({
          disabled={disabled}
          onClick={onClickFunction}
          // decide the design of button according to the props
-         className={`${
-            colorTheme === 'outlined'
-               ? outlinedClasses
-               : colorTheme === 'outlinedPrimary'
-               ? oulinedPrimaryClasses
-               : colorTheme === 'black'
-               ? blackClasses
-               : colorTheme === 'danger'
-               ? dangerClasses
-               : primaryClasses
-         } ${allClasses}`}
+         className={allClasses}
       >
          {loading && (
             <LoadingSpinner
@@ -70,7 +42,7 @@ ButtonBtn.propTypes = {
    onClickFunction: PropTypes.func,
    colorTheme: PropTypes.string,
    modifyClasses: PropTypes.string,
-   theme: PropTypes.string,
+   // theme: PropTypes.string,
    loading: PropTypes.bool,
    disabled: PropTypes.bool,
 };
