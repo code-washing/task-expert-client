17839bc782bb880532a7e1e40370f03e6db88e81
code-washing <tadcassin225@gmail.com>
2024-03-08 19:43:25 +0600

    Add new btn color classes to globals css and update tailwind variables, also change styles of banner btns

--- src/components/page-related/home/Banner/BannerText/BannerTextBtns/BannerTextBtns.jsx
+++ src/components/page-related/home/Banner/BannerText/BannerTextBtns/BannerTextBtns.jsx
@@ -17,11 +17,13 @@ const BannerTextBtns = ({ modifyClasses = '' }) => {
       <div
          className={`flex flex-col xs:flex-row gap-4 xs:gap-3 justify-center items-center lg:justify-start lg:gap-3 ${modifyClasses}`}
       >
-         <LinkBtn text="Let's Explore" url='#learn-more' />
+         <LinkBtn text="Let's Explore" url='#learn-more' modifyClasses='!duration-[300ms]' />
+
          <ButtonBtn
             onClickFunction={openSignupFormWithBackdrop}
             text="Sign Up - It's Free!"
-            colorTheme='primaryOutlined'           
+            colorTheme='primaryOutlined'  
+            modifyClasses='!duration-[300ms]'         
          />
       </div>
    );
