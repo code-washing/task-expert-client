e3d1cba22c290200fb92d52a3fea475c45a6b707
code-washing <tadcassin225@gmail.com>
2024-03-10 22:02:39 +0600

    Refactor Features section styles

--- src/components/page-related/home/Features/Feature/Feature.jsx
+++ src/components/page-related/home/Features/Feature/Feature.jsx
@@ -9,7 +9,7 @@ const Feature = ({ featureData, modifyClasses = '' }) => {
    const { heading, description, icon } = featureData;
 
    return (
-      <div className={`text-center rounded-lg md:rounded-xl lg:rounded-2xl p-6 bg-white border border-neutral-200 shadow-small ${modifyClasses}`}>
+      <div className={`text-center rounded-lg md:rounded-xl lg:rounded-2xl p-6 2md:py-7 bg-white border border-neutral-200 shadow-small ${modifyClasses}`}>
          <FeatureIcon icon={icon} modifyClasses='mx-auto mb-5' />
 
          <h3 className='font-bold text-textPrimary text-xl mb-2'>{heading}</h3>
--- src/components/page-related/home/Features/Features.jsx
+++ src/components/page-related/home/Features/Features.jsx
@@ -8,12 +8,12 @@ const Features = () => {
    return (
       <>
          <h2
-            className={`text-2xl !leading-snug font-bold 2md:text-3xl xl:text-4xl 3xl:text-5xl md:col-span-2 mb-7 md:mb-10 text-center `}
+            className={`text-2xl !leading-snug font-bold 2md:text-3xl xl:text-4xl 3xl:text-5xl md:col-span-2 mb-8 md:mb-10 text-center `}
          >
             Our <span className='text-primary'>Features</span>.
          </h2>
 
-         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 md:gap-14'>
+         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 md:gap-10 xl:gap-12'>
             {features?.map(feature => {
                return <Feature key={feature.id} featureData={feature} />;
             })}
