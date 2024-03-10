495f230454e95b03bb54d172cac8d959835d24fc
code-washing <tadcassin225@gmail.com>
2024-03-10 16:53:12 +0600

    Refactor Carousel1 component

--- src/components/shared/Carousel1/useCarousel1.js
+++ src/components/shared/Carousel1/useCarousel1.js
@@ -1,49 +1,50 @@
 // react
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 
 const useCarousel1 = (imagesData, interval = 3000) => {
-  const lastSlide = imagesData.length - 1;
-  const [curSlide, setCurSlide] = useState(0);
-  const prevSlide = curSlide === 0 ? -1 : curSlide - 1;
-  const nextSlide = curSlide === lastSlide ? -1 : curSlide + 1;
+   const lastIndex = imagesData.length - 1;
+   const firstIndex = 0;
+   const [centerSlide, setCenterSlide] = useState(firstIndex);
+   const leftSlide = centerSlide === firstIndex ? lastIndex : centerSlide - 1;
+   const rightSlide = centerSlide === lastIndex ? firstIndex : centerSlide + 1;
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setCurSlide((prev) => {
-        return prev === lastSlide ? 0 : prev + 1;
-      });
+   useEffect(() => {
+      const timer = setTimeout(() => {
+         setCenterSlide(prev => {
+            return prev === lastIndex ? 0 : prev + 1;
+         });
 
-      clearTimeout(timer);
-    }, interval);
+         clearTimeout(timer);
+      }, interval);
 
-    return () => {
-      clearTimeout(timer);
-    };
-  }, [curSlide, lastSlide, interval]);
+      return () => {
+         clearTimeout(timer);
+      };
+   }, [centerSlide, lastIndex, interval]);
 
-  // go to next slide
-  const goNextSlide = () => {
-    if (curSlide !== lastSlide) {
-      setCurSlide((prev) => prev + 1);
-    }
-  };
+   // go to next slide
+   const goNextSlide = () => {
+      if (centerSlide !== lastIndex) {
+         setCenterSlide(prev => prev + 1);
+      }
+   };
 
-  // go to prev slide
-  const goPrevSlide = () => {
-    if (curSlide !== 0) {
-      setCurSlide((prev) => prev - 1);
-    }
-  };
+   // go to prev slide
+   const goPrevSlide = () => {
+      if (centerSlide !== 0) {
+         setCenterSlide(prev => prev - 1);
+      }
+   };
 
-  return {
-    curSlide,
-    setCurSlide,
-    goNextSlide,
-    goPrevSlide,
-    prevSlide,
-    nextSlide,
-    lastSlide,
-  };
+   return {
+      centerSlide,
+      setCenterSlide,
+      goNextSlide,
+      goPrevSlide,
+      leftSlide,
+      rightSlide,
+      lastIndex,
+   };
 };
 
 export default useCarousel1;
