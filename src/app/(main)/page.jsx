01e186da9a7ecd37586bfb72b69ec931f6b24fea
code-washing <tadcassin225@gmail.com>
2024-03-10 21:54:15 +0600

    Add features section to home page

--- src/app/(main)/page.jsx
+++ src/app/(main)/page.jsx
@@ -1,11 +1,11 @@
 // components
 import InnerContainer from '@/components/containers/InnerContainer/InnerContainer';
 import Banner from '@/components/page-related/home/Banner/Banner';
-import TargetAudience from '@/components/page-related/home/TargetAudience/TargetAudience';
 import LoginModal from '@/components/forms/LoginModal/LoginModal';
 import RegistrationModal from '@/components/forms/RegistrationModal/RegtistrationModal';
 import PasswordResetModal from '@/components/forms/PasswordResetModal/PasswordResetModal';
 import About from '@/components/page-related/home/About/About';
+import Features from '@/components/page-related/home/Features/Features';
 
 const Home = () => {
    return (
@@ -22,9 +22,12 @@ const Home = () => {
             </InnerContainer>
          </section>
 
-         <section id='learn-more' className='mb-sectionGapMd md:mb-sectionGapLg'>
-            <InnerContainer>
-               <TargetAudience />
+         <section
+            id='features'
+            className='mb-sectionGapMd md:mb-sectionGapLg'
+         >
+            <InnerContainer>          
+               <Features />
             </InnerContainer>
          </section>
 
