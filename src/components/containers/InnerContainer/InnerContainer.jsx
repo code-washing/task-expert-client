52e0b43845a0be00ef88e4795240f19108666c85
code-washing <tadcassin225@gmail.com>
2024-03-11 18:40:57 +0600

    Add props for specific side padding in InnerContainer component and delete InnerContainerLeft from project

--- src/components/containers/InnerContainer/InnerContainer.jsx
+++ src/components/containers/InnerContainer/InnerContainer.jsx
@@ -1,19 +1,30 @@
 // react imports
 import PropTypes from 'prop-types';
 
-const InnerContainer = ({ children, modifyClasses = '' }) => {
-  return (
-    <div
-      className={`w-full px-4 xs:px-6 md:px-8 lg:px-10 xl:px-14 2xl:px-20 3xl:px-28 ${modifyClasses}`}
-    >
-      {children}
-    </div>
-  );
+const InnerContainer = ({
+   children,
+   paddingSide = 'both',
+   modifyClasses = '',
+}) => {
+   return (
+      <div
+         className={`w-full ${
+            paddingSide === 'left'
+               ? 'pl-4 xs:pl-6 md:pl-8 lg:pl-10 xl:pl-14 2xl:pl-20 3xl:pl-28'
+               : paddingSide === 'right'
+               ? 'pr-4 xs:pr-6 md:pr-8 lg:pr-10 xl:pr-14 2xl:pr-20 3xl:pr-28'
+               : 'px-4 xs:px-6 md:px-8 lg:px-10 xl:px-14 2xl:px-20 3xl:px-28'
+         }  ${modifyClasses}`}
+      >
+         {children}
+      </div>
+   );
 };
 
 InnerContainer.propTypes = {
-  children: PropTypes.node,
-  modifyClasses: PropTypes.string,
+   children: PropTypes.node,
+   side: PropTypes.string,
+   modifyClasses: PropTypes.string,
 };
 
 export default InnerContainer;
