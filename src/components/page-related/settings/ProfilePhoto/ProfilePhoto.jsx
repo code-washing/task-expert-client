cbb57c1c44381c45ff17dc40f9a5f705d496857f
code-washing <tadcassin225@gmail.com>
2024-03-14 23:23:22 +0600

    Provide a default photo for UserProfile and ProfilePhoto components

--- src/components/page-related/settings/ProfilePhoto/ProfilePhoto.jsx
+++ src/components/page-related/settings/ProfilePhoto/ProfilePhoto.jsx
@@ -3,12 +3,23 @@ import PropTypes from 'prop-types';
 
 // next
 import Image from 'next/image';
+import { Icon } from '@iconify/react';
 
 const ProfilePhoto = ({ imageSource, modifyClasses = '' }) => {
    return (
       <div
          className={`w-32 border border-neutral-300 mx-auto aspect-square rounded-full overflow-hidden ${modifyClasses}`}
       >
+         {!imageSource && (
+            <div className='w-full h-full p-4 flex items-center justify-center'>
+               <Icon
+                  className='w-full h-full text-neutral-300'
+                  icon='mingcute:user-3-fill'
+               />
+            </div>
+         )}
+
+
          {/* image */}
          {imageSource && (
             <Image
