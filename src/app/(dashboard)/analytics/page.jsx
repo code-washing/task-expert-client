f8cb9465f692f77eb05fb3cebe6edbbac48804fa
code-washing <tadcassin225@gmail.com>
2024-04-07 00:49:16 +0600

    Create tasks info section of the analytics page

--- src/app/(dashboard)/analytics/page.jsx
+++ src/app/(dashboard)/analytics/page.jsx
@@ -1,13 +1,19 @@
 // components
 import Charts from '@/components/page-related/analytics/Charts/Charts';
+import TasksInfo from '@/components/page-related/analytics/TasksInfo/TasksInfo';
 
 const page = () => {
    return (
-      <div className='pt-8 md:pt-12 lg:pt-14 xl:pt-16 px-4 md:px-8 xl:px-16'>
-         <h2 className='text-center mb-6 font-bold text-lg lg:text-2xl underline'>Overview</h2>
+      <div className='py-8 md:py-12 lg:py-14 xl:py-16 px-4 md:px-8 xl:px-16'>
+         <h2 className='text-center mb-6 xl:mb-12 font-bold text-lg lg:text-2xl underline'>
+            Overview
+         </h2>
 
          {/* charts  */}
          <Charts />
+
+         {/* task info */}
+         <TasksInfo />
       </div>
    );
 };
---
+++ src/components/page-related/analytics/TasksInfo/TasksInfo.jsx
@@ -0,0 +1,43 @@
+'use client';
+
+// components
+import TasksBlock from '../TasksBlock/TasksBlock';
+import ChartHeading from '../ChartHeading/ChartHeading';
+
+// hook
+import useTaskSeparator from '@/hooks/useTaskSeparator';
+import { useSelector } from 'react-redux';
+
+const TasksInfo = () => {
+   const { totalTasks } = useSelector(store => store.task);
+   const { findTasksByStatus } = useTaskSeparator();
+
+   const todoTasks = totalTasks && findTasksByStatus(totalTasks, 0);
+   const ongoingTasks = totalTasks && findTasksByStatus(totalTasks, 1);
+
+   return (
+      <section className='grid grid-cols-1 lg:grid-cols-2 gap-8'>
+         <div>
+            <ChartHeading text='Todo Tasks (Overview)' />
+
+            <TasksBlock
+               data={todoTasks}
+               label='Todo'
+               modifyClasses='rounded-lg md:rounded-2xl xl:rounded-3xl border border-neutral-200 shadow-small'
+            />
+         </div>
+
+         <div>
+            <ChartHeading text='Ongoing Tasks (Overview)' />
+
+            <TasksBlock
+               data={ongoingTasks}
+               label='Ongoing'
+               modifyClasses='rounded-lg md:rounded-2xl xl:rounded-3xl border border-neutral-200 shadow-small'
+            />
+         </div>
+      </section>
+   );
+};
+
+export default TasksInfo;
