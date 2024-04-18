0c81a40a2d540ff8bf05d7826d9d30c827b59013
code-washing <tadcassin225@gmail.com>
2024-04-18 22:38:01 +0600

    Refactor code for Forms and Button components into a barrel import/export

---
+++ src/components/forms/index.jsx
@@ -0,0 +1,19 @@
+'use client';
+
+// imports
+export { default as FilterForm } from './FilterForm/FilterForm';
+export { default as LoginForm } from './LoginForm/LoginForm';
+export { default as LoginFormWithImage } from './LoginFormWithImage/LoginFormWithImage';
+export { default as LoginModal } from './LoginModal/LoginModal';
+export { default as PasswordResetForm } from './PasswordResetForm/PasswordResetForm';
+export { default as PasswordResetModal } from './PasswordResetModal/PasswordResetModal';
+export { default as RegistrationForm } from './RegistrationForm/RegistrationForm';
+export { default as RegistrationFormWithImage } from './RegistrationFormWithImage/RegistrationFormWithImage';
+export { default as RegistrationModal } from './RegistrationModal/RegistrationModal';
+export { default as SortForm } from './SortForm/SortForm';
+export { default as TaskCreateForm } from './TaskCreateForm/TaskCreateForm';
+export { default as TaskEditForm } from './TaskEditForm/TaskEditForm';
+
+
+
+
