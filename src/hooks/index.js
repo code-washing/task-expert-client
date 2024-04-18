622a29505c21d1a58209f9fe2a29880e1efa146d
code-washing <tadcassin225@gmail.com>
2024-04-18 23:10:26 +0600

    Refactor code for custom hooks into a barrel import/export

---
+++ src/hooks/index.js
@@ -0,0 +1,21 @@
+'use client';
+
+// exports
+export { default as useAuth } from './useAuth';
+export { default as useAxios } from './useAxios';
+export { default as useClickOutside } from './useClickOutside';
+export { default as useDashboardMobileNav } from './useDashboardMobileNav';
+export { default as useEnterPress } from './useEnterPress';
+export { default as useFirebaseMethods } from './useFirebaseMethods';
+export { default as useFormVisiblity } from './useFormVisiblity';
+export { default as useGetInitialTasksData } from './useGetInitialTasksData';
+export { default as useLoginMethods } from './useLoginMethods';
+export { default as useMediaQueryMatcher } from './useMediaQueryMatcher';
+export { default as useMobileNavigation } from './useMobileNavigation';
+export { default as useProcessChartsData } from './useProcessChartsData';
+export { default as useRedux } from './useRedux';
+export { default as useRegistrationForm } from './useRegistrationForm';
+export { default as useResetForm } from './useResetForm';
+export { default as useStopScrolling } from './useStopScrolling';
+export { default as useTaskDatabaseMethods } from './useTaskDatabaseMethods';
+export { default as useTaskSeparator } from './useTaskSeparator';
