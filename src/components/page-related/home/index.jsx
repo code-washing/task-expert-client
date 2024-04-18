28dc85f20548ebac69a692008b21086e290be8c3
code-washing <tadcassin225@gmail.com>
2024-04-19 00:46:48 +0600

    Refactor code for page related components into their respective barrel import/export

---
+++ src/components/page-related/home/index.jsx
@@ -0,0 +1,8 @@
+'use client';
+
+// exports
+export { default as About } from './About/About';
+export { default as Banner } from './Banner/Banner';
+export { default as Faq } from './Faq/Faq';
+export { default as Features } from './Features/Features';
+export { default as TargetAudience } from './TargetAudience/TargetAudience';
---
+++ src/components/page-related/manage-tasks/index.jsx
@@ -0,0 +1,8 @@
+'use client';
+
+// exports
+export { default as AllTasks } from './AllTasks/AllTasks';
+export { default as StatusSpecificTasks } from './StatusSpecificTasks/StatusSpecificTasks';
+export { default as Task } from './Task/Task';
+export { default as TaskDetailsPanel } from './TaskDetailsPanel/TaskDetailsPanel';
+export { default as TaskUtilsHeader } from './TaskUtilsHeader/TaskUtilsHeader';
